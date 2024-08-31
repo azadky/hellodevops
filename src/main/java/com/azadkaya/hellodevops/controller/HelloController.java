4136b3d971052d621a5e1f57bc8d5ca1c1f747a9
azadky <azadky@gmail.com>
2024-08-31 15:14:26 +0300

    localdate added

--- src/main/java/com/azadkaya/hellodevops/controller/HelloController.java
+++ src/main/java/com/azadkaya/hellodevops/controller/HelloController.java
@@ -3,13 +3,16 @@ package com.azadkaya.hellodevops.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+
 @RestController
 public class HelloController {
 
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World From Project";
+
+        return "Hello World From Project " + LocalDate.now();
     }
 
 
